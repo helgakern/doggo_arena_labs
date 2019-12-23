51fb60641b64c0abd479b581712f86f76c8544cf
Hano <hinsul3113@gmail.com>
2019-12-23 14:02:11 -0800

    Add doggoArena project

---
+++ main.js
@@ -0,0 +1,7 @@
+// JS: Events & The Loop
+
+// Events
+const toxicTeam = document.querySelector("#toxic-tim");
+const teamSalmon = document.querySelector(".team.salmon");
+
+// <selected-node>.addEventListener
