149f366658a11bd092454df0b8e6606725e8bce2
helgakern <helgakern@gmail.com>
2020-05-13 14:28:14 -0700

    finished

--- main.js
+++ main.js
@@ -128,34 +128,119 @@ document.addEventListener("mousemove", (event) => {
 
 // Form & Input Events
 // Demo: Type Loudly & Explode on Submit
-const random = (n) => Math.ceil(Math.random() * n);
-const keySound = () => new Audio(`sounds/vintage-keyboard-${random(4)}.wav`);
+if (false) {
+    const random = (n) => Math.ceil(Math.random() * n);
+    const keySound = () => new Audio(`sounds/vintage-keyboard-${random(4)}.wav`);
 
-document.querySelectorAll("input").forEach((inputNode) => {
-    inputNode.addEventListener("input", (event) => {
-        keySound().play();
+    document.querySelectorAll("input").forEach((inputNode) => {
+        inputNode.addEventListener("input", (event) => {
+            keySound().play();
+        });
     });
-});
 
-const explosionSound = () => new Audio("sounds/small-explosion.wav");
-document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    // preventDefault prevents the form from being submitted
-    //   This prevents the forms default behaviour
-    // When used with forms, the form data will not be submitted somewhere
-    //when used with links, the href will not be followed
-    explosionSound().play();
-});
+    const explosionSound = () => new Audio("sounds/small-explosion.wav");
+    document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        // preventDefault prevents the form from being submitted
+        //   This prevents the forms default behaviour
+        // When used with forms, the form data will not be submitted somewhere
+        //when used with links, the href will not be followed
+        explosionSound().play();
+    });
+}
 
 // Exercise: Applicant's Avatar
-const applicantPreview = document.querySelector(
-    "#applicant-preview .doggo.blank"
-);
+if (false) {
+    const applicantPreview = document.querySelector(
+        "#applicant-preview .doggo.blank"
+    );
 
-document
-    .querySelector('input[name="picture-url"]')
-    .addEventListener("input", (event) => {
-        const imageUrl = event.currentTarget.value;
-        //   console.log("imageUrl: ", event.currentTarget.value);
-        applicantPreview.style.backgroundImage = `url(${imageUrl})`;
-    });
+    document
+        .querySelector('input[name="picture-url"]')
+        .addEventListener("input", (event) => {
+            const imageUrl = event.currentTarget.value;
+            //   console.log("imageUrl: ", event.currentTarget.value);
+            applicantPreview.style.backgroundImage = `url(${imageUrl})`;
+        });
+}
+
+// Create Doggo on submit
+document.addEventListener("DOMContentLoaded", () => {
+    document
+        .querySelector("#application-form")
+        .addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const formNode = event.currentTarget;
+            // To easily access all of the input field values within a form
+            // use the FormData constructor like below, passing it the form node
+            // as an argument
+            const formData = new FormData(formNode);
+            console.log(formData);
+            // debugger;
+            // Use 'debugger' to interrupt a program in a browser to execute in the same scope
+            // as the line where the debugger was written.
+            // This is similar to 'byebug' or 'binding.pry' for Ruby.
+            // It will only work in your developer console.
+
+            // To get the value of the input fields in the form by their name, we use
+            // the 'get' method on the formData instance
+            formData.get("name");
+            formData.get("picture-url");
+            formData.get("team-name");
+            // We can use formData.entries() to get an 'iterator' for looping
+            // over the input fields using a for...of loop
+            for (let field of formData.entries()) {
+                console.log("input name: ", field[0], "input value: ", field[1]);
+            }
+
+            // Transform that 'iterator' into an array
+            // Array.from(formData.entries())
+            // and then we can use forEach, map, etc
+            const blankDoggo = document.querySelector(
+                "#applicant-preview .doggo.blank"
+            );
+
+            blankDoggo.style.backgroundImage = `url(${formData.get("picture-url")})`;
+            blankDoggo.querySelector("h1").innerHTML = formData.get("name");
+            blankDoggo.style.border = `medium solid ${formData.get("team-name")}`;
+        });
+});
+
+// Keyboard Events
+let lettersTyped = "";
+document.addEventListener("keypress", (event) => {
+    lettersTyped += event.key;
+    // console.log("lettersTyped: ", lettersTyped);
+    const last5lettersTyped = lettersTyped.slice(lettersTyped.length - 5);
+    if (last5lettersTyped === "panic") {
+        window.location.replace("http://hackertyper.net");
+    }
+});
+
+document.addEventListener("keydown", (event) => {
+    console.log("event: ", event);
+    //   const currentTarget = event.currentTarget;
+    //   const target = event.target;
+    //   const keyCode = event.keyCode;
+    //   const altKey = event.altKey;
+    //   const shiftKey = event.shiftKey;
+    //   const metaKey = event.metaKey;
+    //   const key = event.key;
+
+    // destructuring the above 7 lines into the line below
+    const {
+        currentTarget,
+        target,
+        keyCode,
+        altKey,
+        shiftKey,
+        metaKey,
+        key,
+    } = event;
+
+    console.log("keyCode: ", keyCode);
+    if (altKey && shiftKey && keyCode === 78) {
+        window.location.href = "http://nyan.cat";
+    }
+});
